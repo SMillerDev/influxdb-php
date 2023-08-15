b710d2b8e7726d8a99158e7a29b7be27f189a4c9
Sean Molenaar <sean@seanmolenaar.eu>
2023-08-15 15:47:39 +0200

    fix: add classes to allow V2 precision

---
+++ src/InfluxDB/Precision/InfluxDBV1Precision.php
@@ -0,0 +1,11 @@
+<?php
+namespace InfluxDB\Precision;
+
+class InfluxDBV1Precision extends InfluxDBPrecision
+{
+    /**
+     * Precision constants
+     */
+    public const PRECISION_NANOSECONDS = 'n';
+    public const PRECISION_MICROSECONDS = 'u';
+}
